e4c74685c89383a1bd967812ffa988a9e9983b14
Shanthanu S Balekai <97695182+funky-SB-stubbz@users.noreply.github.com>
2023-03-18 20:30:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import * as React from "react";
+// import Grid from "./components/Grid/Grid";
+import BottomBar from "./components/Bottombar/Bottom_bar";
+// import { NavLink } from "react-router-dom";
+// import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+// import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SUT from "./pages/SUT";
+import SIM from "./pages/SIM";
+import Analyzer from "./pages/Analyzer";
+import Messages from "./pages/Messages";
+import Comment from "./pages/Comment";
+import Timer from "./pages/Timer";
+import DataTransfer from "./pages/DataTransfer";
+// import Grid from "./components/Grid/Grid";
+// import { useState } from "react";
+
+function App() {
+  // const [state, changeState] = useState("Home");
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        {/* <Grid text="SUT" /> */}
+        <Routes>
+          <Route exact path="/" element={<SUT />} />
+          <Route path="SUT" element={<SUT />} />
+          <Route path="SIM" element={<SIM />} />
+          <Route path="Analyzer" element={<Analyzer />} />
+          <Route path="Timer" element={<Timer />} />
+          <Route path="Comment" element={<Comment />} />
+          <Route path="Messages" element={<Messages />} />
+          <Route path="DataTransfer" element={<DataTransfer />} />
+        </Routes>
+        <BottomBar />
+      </Router>
+    </>
+  );
+}
+
+export default App;
