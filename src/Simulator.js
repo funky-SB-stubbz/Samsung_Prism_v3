543bf9115fc35c13bb695eeb476857783e3bdcec
Shanthanu S Balekai <97695182+funky-SB-stubbz@users.noreply.github.com>
2023-03-20 21:33:51 +0530

    Update and rename App.js to Simulator.js

--- src/Simulator.js
+++ src/Simulator.js
@@ -1,12 +1,10 @@
 import "./styles.css";
 import * as React from "react";
-// import Grid from "./components/Grid/Grid";
 import BottomBar from "./components/Bottombar/Bottom_bar";
 // import { NavLink } from "react-router-dom";
 // import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-// import {BrowserRouter as Router} from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SUT from "./pages/SUT";
 import SIM from "./pages/SIM";
@@ -15,17 +13,16 @@ import Messages from "./pages/Messages";
 import Comment from "./pages/Comment";
 import Timer from "./pages/Timer";
 import DataTransfer from "./pages/DataTransfer";
-// import Grid from "./components/Grid/Grid";
-// import { useState } from "react";
 
-function App() {
-  // const [state, changeState] = useState("Home");
+
+function Simulator() {
+ 
   return (
     <>
       <Router>
         <Navbar />
         <Sidebar />
-        {/* <Grid text="SUT" /> */}
+       
         <Routes>
           <Route exact path="/" element={<SUT />} />
           <Route path="SUT" element={<SUT />} />
@@ -42,4 +39,4 @@ function App() {
   );
 }
 
-export default App;
+export default Simulator;
